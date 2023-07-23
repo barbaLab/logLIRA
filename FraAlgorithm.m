7de1b21cb2eee55ca32abe67e5a3cd87859182c8
Francesco Negri <francesco.negri@outlook.com>
2023-07-23 11:08:10 -0500

    Fix

--- FraAlgorithm.m
+++ FraAlgorithm.m
@@ -1,3 +1,3 @@
 function output = FraAlgorithm(sig, p)
-    output = logssar(sig, p.StimI, p.fs, p.FRA.blankingPeriod, 'SaturationVoltage', p.FRA.saturationVoltage);
+    output = logssar(sig, p.StimI, p.fs, p.FRA.blankingPeriod);
 end
